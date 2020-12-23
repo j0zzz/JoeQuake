018cbfca0a33136ad059e62ca668dc2122b8600f
j0zzz <57400093+j0zzz@users.noreply.github.com>
2020-12-23 17:34:41 +0100

    removed 'beta' naming from version

--- trunk/version.h
+++ trunk/version.h
@@ -20,7 +20,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 // version.h
 
 #define	SERVER_VERSION		1.09
-#define	JOEQUAKE_VERSION	"0.17.0 beta"
+#define	JOEQUAKE_VERSION	"0.17.0"
 
 int build_number (void);
 void Host_Version_f (void);
