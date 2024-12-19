109352cadef2b84ff95473733d98488f48343bab
j0zzz <joequake@gmail.com>
2024-12-19 23:24:55 +0100

    Include net_messagetimeout.defaultvalue in movekeys stufftext
    To make sure we don't set the cvar to 0.0 in older clients.

--- trunk/sv_user.c
+++ trunk/sv_user.c
@@ -456,6 +456,7 @@ void SV_ReadClientMove (usercmd_t *move)
 		host_client->edict->v.impulse = i;
 }
 
+extern	cvar_t		net_messagetimeout;
 extern	kbutton_t	in_moveleft, in_moveright, in_forward, in_back, in_jump;
 
 /*
@@ -484,7 +485,7 @@ nextmsg:
 		MSG_BeginReading ();
 
 		//joe: send movement keys
-		Host_ClientCommands("net_messagetimeout mk%i%i%i%i%i\n", in_forward.state, in_back.state, in_moveleft.state, in_moveright.state, in_jump.state);
+		Host_ClientCommands("net_messagetimeout %smk%i%i%i%i%i\n", net_messagetimeout.defaultvalue, in_forward.state, in_back.state, in_moveleft.state, in_moveright.state, in_jump.state);
 
 		while (1)
 		{
